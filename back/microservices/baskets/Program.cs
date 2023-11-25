6c6ea7adc313f09ccab7b6b8bfaed726a516d399
Louis Teilliais <louisteilliais@mbp-de-louis.home>
2023-11-25 18:25:27 +0100

    add docker-compose

--- back/microservices/baskets/Program.cs
+++ back/microservices/baskets/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(configuration.GetConnectionString("Default"));
+});
+
+// TODO Implémenter services
+
+// Todo Implémenter repository
+
 
 var app = builder.Build();
 
