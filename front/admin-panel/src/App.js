899d80ddb252addd0b5e725269b553f26eccaa8d
taobourmaud <tao.bourmaud@ynov.com>
2023-12-06 23:55:59 +0100

    test suppr erreur Keycloak

--- front/admin-panel/src/App.js
+++ front/admin-panel/src/App.js
@@ -1,10 +1,13 @@
 import { Admin, Resource } from "react-admin";
 import { ProductList, ProductEdit, ProductCreate } from './components/Products';
 import restProvider from 'ra-data-simple-rest';
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloakClient from "./components/Keycloak";
 
 const dataProvider = restProvider('http://localhost:3000');
 function App() {
   return (
+    <ReactKeycloakProvider authClient={keycloakClient}>
       <Admin dataProvider={dataProvider}>
         <Resource
           name="products"
@@ -13,6 +16,7 @@ function App() {
           create={ProductCreate}
         />
       </Admin>
+    </ReactKeycloakProvider>
     );
   }
 export default App;
